bd590214cf444f9501d71ddb98835cac26ce7d33
Sergey Anisimov <sanisimov@moikot.com>
2019-07-10 20:32:17 +0200

    Register Prometheus gauge vector

--- metric_recorder.go
+++ metric_recorder.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/moikot/smartthings-metrics/extractors"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 type MetricRecorder interface {
@@ -59,5 +60,5 @@ func newGaugeVec(attr *extractors.AttributeValue) *prometheus.GaugeVec {
 		labelNames = append(labelNames, name)
 	}
 
-	return prometheus.NewGaugeVec(opts, labelNames)
+	return promauto.NewGaugeVec(opts, labelNames)
 }
